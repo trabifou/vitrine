ba7ffd12215d751389cf1e52913bbe897471c01a
trabifou <jbland.59000@yahoo.fr>
2025-12-03 15:10:14 +0100

    presentation contenu ok

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -6,10 +6,10 @@ const Presentation = () => {
     <Section id="presentation" className="presentation-section">
       <div className="presentation-background"></div>
       <div className="presentation-container">
-        <h1>Offres Canopéerenne</h1>
-        <p className="subtitle">Des solutions adaptées à vos besoins.</p>
+        <h2>Notre Offre</h2>
+        <p className="subtitle"><strong>Des solutions adaptées à vos besoins.</strong></p>
 
-        <div className="presentation-columns">
+        <div className="presentation-rows">
           <div className="presentation-content">
             <p className="presentation-text">
               Canopérenne vous accompagne dans la création de solutions numériques fiables. <br />
@@ -19,7 +19,7 @@ const Presentation = () => {
 
           <div className="presentation-offers">
             <div className="offer-card">
-              <div className="card-head">
+              <div className="card-head offer-1">
                 <h3>Starter</h3>
                 <p><i>à partir de 700 €</i></p>
               </div>
@@ -35,9 +35,9 @@ const Presentation = () => {
             </div>
 
             <div className="offer-card">
-              <div className="card-head">
+              <div className="card-head offer-2">
                 <h3>Pro</h3>
-                <p><i>à partir de 1400 €</i></p>
+                <p><i>à partir de 1500 €</i></p>
               </div>
 
               <ul className="card-traits">
@@ -51,7 +51,7 @@ const Presentation = () => {
             </div>
 
             <div className="offer-card">
-              <div className="card-head">
+              <div className="card-head offer-3">
                 <h3>Dev à la Tâche</h3>
                 <p><i>360 €/jour</i></p>
               </div>
